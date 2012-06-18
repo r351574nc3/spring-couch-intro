65932cf346fdd37cdbac1936997a2cbe63d2f94b
Przybylski 중광 <leo@rsmart.com>
2012-06-17 17:00:40 -0700

    Removing JSON stuff

--- src/main/java/com/clearboxmedia/couchspring/EventService.java
+++ src/main/java/com/clearboxmedia/couchspring/EventService.java
@@ -1,9 +1,35 @@
-package com.clearboxmedia.srs.rest;
+/*
+ * Copyright (c) 2012, Warner Onstine and Leo Przybylski
+ * All rights reserved.
+ * 
+ * - Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ * 
+ * - Redistributions of source code must retain the above copyright notice, this list of 
+ * conditions and the following disclaimer.
+ * 
+ * - Redistributions in binary form must reproduce the above copyright notice, this list 
+ * of conditions and the following disclaimer in the documentation and/or other materials 
+ * provided with the distribution.
+ * 
+ * - Neither the name of the <ORGANIZATION> nor the names of its contributors may be used
+ * to endorse or promote products derived from this software without specific prior written 
+ * permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS 
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.clearboxmedia.couchspring;
 
-import com.clearboxmedia.srs.couch.PersistanceService;
-import com.clearboxmedia.srs.domain.EventWrapper;
-import com.clearboxmedia.srs.json.SearchService;
-import com.clearboxmedia.srs.search.EventSearchCriteria;
+import com.clearboxmedia.couchspring.couch.PersistanceService;
+import com.clearboxmedia.couchspring.domain.EventWrapper;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,11 +49,6 @@ import org.jcouchdb.db.Database;
 public class EventService {
     @Autowired
     private PersistanceService persistanceService;
-
-
-    @Autowired
-    private SearchService searchService;
-
 
     public void setPersistanceService(final PersistanceService persistanceService) {
         this.persistanceService = persistanceService;
