77db59311a011260c05fd118b9edcaa29ef8bfb0
Warner Onstine <warner@warneronstine.com>
2012-06-17 20:42:06 -0700

    first view test draft

---
+++ src/test/java/com/clearboxmedia/couchspring/it/CouchViewTest.java
@@ -0,0 +1,52 @@
+package com.clearboxmedia.couchspring.it;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.jcouchdb.db.Options;
+import org.jcouchdb.document.ViewResult;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.clearboxmedia.couchspring.couch.PersistanceService;
+import com.clearboxmedia.couchspring.domain.Event;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/root-context.xml")
+@Component
+public class CouchViewTest {
+	
+	@Autowired
+    private PersistanceService persistanceService;
+	
+	public void testListingQuery() {
+		String viewName = "event/list";
+		ViewResult<Event> results = persistanceService.getDatabase().queryView(viewName, Event.class, null, null);
+	}
+	
+	public void testQueryByVenueId() {
+		String viewName = "event/allByVenueId";
+		
+		ViewResult<Event> results = persistanceService.getDatabase().queryViewByKeys(viewName, Event.class, Arrays.asList("id"), null, null);
+		
+	}
+	
+	public void testQueryByDate() {
+		String viewName = "event/allByDate";
+		List<String> keys = new ArrayList<String>();
+		keys.add("year");
+		keys.add("month");
+		keys.add("day");
+		Options opts = new Options();
+		opts.startKey(keys);
+		opts.endKey("\u9999");
+		
+		ViewResult<Event> results = persistanceService.getDatabase().queryViewByKeys(viewName, Event.class, null, opts, null);
+		
+	}
+
+}
