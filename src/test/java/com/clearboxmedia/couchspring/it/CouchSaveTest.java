17b6ecf966569e4d9281b48c7f2edc38e51b66b3
Warner Onstine <warner@warneronstine.com>
2012-06-17 19:56:37 -0700

    renaming packages to match rest of project

--- src/test/java/com/clearboxmedia/couchspring/it/CouchSaveTest.java
+++ src/test/java/com/clearboxmedia/couchspring/it/CouchSaveTest.java
@@ -26,7 +26,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package com.clearboxmedia.springcouch.it;
+package com.clearboxmedia.couchspring.it;
 
 import org.junit.*;
 
